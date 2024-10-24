1324632249732bdbb67c13c1dba57e7ab7407946
Vinay Krishna <vinaykrishnakolar@gmail.com>
2024-10-24 10:13:41 +0530

    Till Selenium025

--- src/Test/Java/ex_selenium/ex_19102024/Selenium026.java
+++ src/Test/Java/ex_selenium/ex_19102024/Selenium026.java
@@ -1,4 +1,20 @@
 package ex_selenium.ex_19102024;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
 public class Selenium026 {
+
+    @Test
+    public void testVWOLoginNegative(){
+        // ID, ClassName,Name, Link and Partial is done.
+
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://awesomeqa.com/practice.html");
+        driver.findElement(By.id("sex-0")).click();
+
+        // TagName and Xpath and CSS selector
+    }
 }
