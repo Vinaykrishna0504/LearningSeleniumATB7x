d23c3cc36bf49fb009fb8166ad7b62cb4afcb723
Vinay Krishna <vinaykrishnakolar@gmail.com>
2024-11-08 09:01:16 +0530

    Till Selenium029Ebay

--- src/Test/Java/ex_selenium/ex_20102024/Selenium029Ebay.java
+++ src/Test/Java/ex_selenium/ex_20102024/Selenium029Ebay.java
@@ -1,10 +1,14 @@
 package ex_selenium.ex_20102024;
 
 import io.qameta.allure.Description;
+import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Selenium029Ebay {
 
@@ -24,5 +28,36 @@ public class Selenium029Ebay {
             throw new RuntimeException(e);
         }
 
+        //#gh-ac
+        // -> Xpath Selector - //input[@id='gh-ac'], CSS selector -> input[id='gh-ac']
+        WebElement searchbox = driver.findElement(By.cssSelector("[id='gh-ac']"));
+        searchbox.sendKeys("macmini");
+
+        WebElement searchBoxbutton = driver.findElement(By.cssSelector("[id='gh-btn']"));
+        searchBoxbutton.click();
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Find all the titles
+        // CSS selector -> s-item__title, -> xpath -> div[@class='s-item__title']/span
+        List<WebElement> searchTitles = driver.findElements(By.xpath("// div[@class='s-item__title']/span"));
+        // CSS selector -> s-item__price
+        List<WebElement> searchTitlePrices = driver.findElements(By.xpath("//span[@class='s-item__price']"));
+
+        //min -> Loop where
+        int size = Math.min(searchTitles.size(),searchTitlePrices.size());
+        for (int i = 0;i< size; i++ ){
+            System.out.println("Title:" + searchTitles.get(i).getText()+ " || " + "Price:" + searchTitlePrices.get(i).getText());
+            System.out.println();
+
+            //Max price or Min price - Print it
+
+            // Same above try in flipkart, search iphone 15 pro and print tilt and prices
+
+        }
     }
 }
